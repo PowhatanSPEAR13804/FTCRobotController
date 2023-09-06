801d9654b23ad0bb5d41f2b4115038ce2a25e554
PowhatanSPEAR <phs-roboticsspear@powhatan.k12.va.us>
2023-09-06 16:57:13 -0400

    Started example drive train script and declared some variables inside it.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleop.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class teleop extends LinearOpMode {
+    public void runOpMode() throws InterruptedException {
+        //declare and set motor variables
+        DcMotor frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor;
+        frontLeftMotor = hardwareMap.get(DcMotor.class,"Hub1_Motor0");
+        frontRightMotor = hardwareMap.dcMotor.get("Hub1_Motor1");
+        backLeftMotor = hardwareMap.dcMotor.get("Hub1_Motor2");
+        backRightMotor = hardwareMap.dcMotor.get("Hub1_Motor3");
+
+        //declare gamepad axis variables
+        double x, y, rx;
+
+    }
+}
