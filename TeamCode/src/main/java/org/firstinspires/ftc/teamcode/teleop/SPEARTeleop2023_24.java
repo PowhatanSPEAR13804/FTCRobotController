be19227d9ca69bddbbb1f4d665ea4f8c76484466
PowhatanSPEAR13804 <phs-roboticsspear@powhatan.k12.va.us>
2023-10-30 15:11:02 -0400

    added throughput

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SPEARTeleop2023_24.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SPEARTeleop2023_24.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 
 //@Disabled
 
@@ -20,17 +19,15 @@ public class SPEARTeleop2023_24 extends LinearOpMode {
         DcMotor motorFrontRight = hardwareMap.dcMotor.get("Hub1_Motor0");
         DcMotor motorBackRight = hardwareMap.dcMotor.get("Hub1_Motor3");
 
-
         DcMotor fourBar = hardwareMap.dcMotor.get("Hub1_Motor2");
 
-        CRServo throughput = (CRServo) hardwareMap.servo.get("Hub2_Servo0");
+        CRServo throughput = (CRServo) hardwareMap.servo.get("Hub1_Servo4");
 
         DcMotor viper = hardwareMap.dcMotor.get("Hub1_Motor1");
 
         CRServo intakeLeft = (CRServo) hardwareMap.servo.get("Hub1_Servo0");
         CRServo intakeRight = (CRServo) hardwareMap.servo.get("Hub1_Servo1");
         // Work in progress CRServo output = (CRServo) hardwareMap.servo.get("Hub1_Servo3");
-
 
         // Reverse the right side motors
         // Reverse left motors if you are using NeveRests
@@ -62,49 +59,36 @@ public class SPEARTeleop2023_24 extends LinearOpMode {
             motorFrontRight.setPower(frontRightPower);
             motorBackRight.setPower(backRightPower);
 
+            //four bar
             double fourBarPower = 0;
             if (gamepad1.dpad_up || gamepad2.dpad_up)
                 fourBar.setPower(Math.min(1,(fourBarPower += 0.001)));
             if(gamepad1.dpad_down||gamepad2.dpad_down)
                 fourBar.setPower(Math.max(0, (fourBarPower -= 0.001)));
 
+            //throughput
+            double tpPower = 0;
+            boolean on = false;
+            if(gamepad2.dpad_left && !on)
+                on = true;
+            if(on)
+                tpPower = Math.min(1, tpPower += 0.02);
+            if(gamepad2.dpad_right && on)
+                on = false;
+            if(!on)
+                tpPower = Math.max(0, tpPower -= 0.02);
+            throughput.setPower(tpPower);
 
-
-            boolean forward = false;
-            //throughput forward (into the robot)
-            if (gamepad2.dpad_left && !forward) {
-                forward = true;
-                throughput.setPower(0.2);
-            }
-            if (gamepad2.dpad_left && forward) {
-                forward = false;
-                throughput.setPower(0);
-            }
-
-            boolean backward = false;
-            //throughput backward (out of the robot)
-            if (gamepad2.dpad_right && !backward) {
-                backward = true;
-                throughput.setPower(-0.2);
-            }
-            if (gamepad2.dpad_right && backward) {
-                backward = false;
-                throughput.setPower(0);
-
-            }
-
+            //viper slide
             double viperPower = 0;
             if (gamepad1.left_trigger>0)
                 viper.setPower(Math.min(1,(viperPower += 0.001)));
             if(gamepad1.right_trigger>0)
                 viper.setPower(Math.max(0, (viperPower -= 0.001)));
 
-
-
-
             telemetry.addData("I see" ,gamepad2.dpad_up);
 
-
+            //intake
             if(gamepad1.x)
             {
                 intakeLeft.setPower(1);
@@ -114,8 +98,6 @@ public class SPEARTeleop2023_24 extends LinearOpMode {
                 intakeLeft.setPower(0);
                 intakeRight.setPower(0);
             }
-
-
 
         }
     }
