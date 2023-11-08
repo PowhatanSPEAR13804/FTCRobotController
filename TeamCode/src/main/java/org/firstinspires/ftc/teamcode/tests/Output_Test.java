e20eb9a0adb023ff24ada720c4108607bd409906
PowhatanSPEAR13804 <phs-roboticsspear@powhatan.k12.va.us>
2023-11-08 16:46:15 -0500

    Improved Controller_Test.java code formatting, added throughput and intake tests to Output_Test.java.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/Output_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/Output_Test.java
@@ -4,30 +4,45 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-//import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.Servo;
 
 //@Disabled
 //safety :)
 
 @TeleOp
-public class Output_Test extends LinearOpMode {
+public class Output_Test extends LinearOpMode{
     @Override
-    public void runOpMode() throws InterruptedException {
-        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("Hub2_Motor0");
-        DcMotor motorBackLeft = hardwareMap.dcMotor.get("Hub2_Motor1");
-        DcMotor motorFrontRight = hardwareMap.dcMotor.get("Hub2_Motor2");
-        DcMotor motorBackRight = hardwareMap.dcMotor.get("Hub2_Motor3");
+    public void runOpMode() throws InterruptedException{
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("H2M0");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("H2M1");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("H2M2");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("H2M3");
+
+        Servo throughput = /*(CRServo)*/ hardwareMap.servo.get("H2S0");
+        //Servo intakeLeft = /*(CRServo)*/ hardwareMap.servo.get("H2S0");
+        //Servo intakeRight = /*(CRServo)*/ hardwareMap.servo.get("H2S1");
 
         motorFrontRight.setDirection(DcMotorSimple.Direction.FORWARD);
         motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBackRight.setDirection(DcMotorSimple.Direction.FORWARD);
         motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        //intakeLeft.setDirection((Servo.Direction.REVERSE));
+        //intakeRight.setDirection((Servo.Direction.FORWARD));
+
+        double throughputPosition = 0.5;
+        boolean forward = false;
+        boolean backward = false;
+        boolean gamepadLeftPressedLastTime = false;
+        boolean gamepadRightPressedLastTime = false;
+
+        double intakePosition = 0.5;
+
         waitForStart();
 
         if (isStopRequested()) return;
 
-        while (opModeIsActive()) {
+        while (opModeIsActive()){
             double y = -gamepad1.left_stick_y; // Remember, this is reversed!
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
@@ -43,10 +58,51 @@ public class Output_Test extends LinearOpMode {
             motorFrontRight.setPower(frontRightPower);
             motorBackRight.setPower(backRightPower);
 
-            telemetry.addLine(""+frontLeftPower);
-            telemetry.addLine(""+backLeftPower);
-            telemetry.addLine(""+frontRightPower);
-            telemetry.addLine(""+backRightPower);
+            //need to make throughput and intake inputs more reliable
+            //throughput
+            if(gamepad1.dpad_right && !forward && !backward && !gamepadLeftPressedLastTime){
+                forward = true;
+            }else if(gamepad1.dpad_right && forward){
+                forward = false;
+            }
+            if(gamepad1.dpad_left && !backward && !forward && !gamepadRightPressedLastTime){
+                backward = true;
+            }else if(gamepad1.dpad_left && backward){
+                backward = false;
+            }
+
+            gamepadLeftPressedLastTime = gamepad1.dpad_left;
+            gamepadRightPressedLastTime = gamepad1.dpad_right;
+
+            if (forward)
+                throughputPosition = 1;
+            if (backward)
+                throughputPosition = 0;
+            if (!forward && !backward)
+                throughputPosition = 0.5;
+            throughput.setPosition(throughputPosition);
+
+            //intake
+            if (gamepad1.x && intakePosition == 0.5){
+                intakePosition = 1;
+            }else if(gamepad1.x && intakePosition != 0.5){
+                intakePosition = 0.5;
+            }
+            if (gamepad1.b && intakePosition == 0.5){
+                intakePosition = 0;
+            }else if(gamepad1.b && intakePosition != 0.5){
+                intakePosition = 0.5;
+            }
+            //intakeLeft.setPosition(intakePosition);
+            //intakeRight.setPosition(intakePosition);
+
+            telemetry.addLine("Front left power: "+frontLeftPower);
+            telemetry.addLine("Back left power: "+backLeftPower);
+            telemetry.addLine("Front right power: "+frontRightPower); //why is this one negative at rest
+            telemetry.addLine("Back right power: "+backRightPower);
+            telemetry.addLine("\nThroughput position: "+throughputPosition);
+            telemetry.addLine("\nIntake position: "+intakePosition);
+            telemetry.update();
         }
     }
 }
