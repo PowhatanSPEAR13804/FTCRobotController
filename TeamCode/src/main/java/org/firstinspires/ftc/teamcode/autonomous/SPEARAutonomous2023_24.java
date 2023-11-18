dfdd675b47b86893d6f63146385b5067a6d4dbe1
PowhatanSPEAR13804 <phs-roboticsspear@powhatan.k12.va.us>
2023-11-18 10:46:35 -0500

    renamed autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SPEARAutonomous2023_24.java
+++
@@ -1,6 +0,0 @@
-package org.firstinspires.ftc.teamcode.autonomous;
-
-public class SPEARAutonomous2023_24 {
-
-
-}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SimpleForward.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="SimpleForward", group="Autonomous")
+
+public class SimpleForward {
+
+}
