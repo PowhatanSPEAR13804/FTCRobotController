1b5b9fd42e31b7418818896838d41eee4bcc0055
PowhatanSPEAR13804 <phs-roboticsspear@powhatan.k12.va.us>
2023-11-18 11:47:53 -0500

    simpleforward and simpleright autonomous edits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SimpleRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SimpleRight.java
@@ -4,15 +4,9 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
-import com.qualcomm.robotcore.hardware.NormalizedRGBA;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
 @Autonomous(name="SimpleForward", group="Autonomous")
 
-public class SimpleForward extends LinearOpMode{
+public class SimpleRight extends LinearOpMode{
     private DcMotor frontRightDrive;
     private DcMotor frontLeftDrive;
     private DcMotor backRightDrive;
@@ -30,19 +24,19 @@ public class SimpleForward extends LinearOpMode{
         backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         backRightDrive.setDirection(DcMotor.Direction.REVERSE);
 
+        frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         waitForStart();
 
-        frontRightDrive.setPower(0.25);
-        frontLeftDrive.setPower(0.25);
-        backRightDrive.setPower(0.25);
-        backLeftDrive.setPower(0.25);
-
-        sleep(3000);
-
-        frontRightDrive.setPower(0);
-        frontLeftDrive.setPower(0);
-        backRightDrive.setPower(0);
-        backLeftDrive.setPower(0);
+        
     }
 }
 
