997a07cab50d0319b163b3f07f8019489e16fbb1
PowhatanSPEAR13804 <phs-roboticsspear@powhatan.k12.va.us>
2023-11-30 15:15:21 -0500

    stuff with pixels in simpleForward!!! (not tested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SimpleForward.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SimpleForward.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
 import org.firstinspires.ftc.teamcode.helperclasses.robotMove;
 
 // TODO - stuff with pixels
@@ -12,11 +14,17 @@ public class SimpleForward extends LinearOpMode{
 
     @Override
     public void runOpMode() {
+        Servo outputS =  hardwareMap.servo.get("Hub1_Servo5");
+        Servo outputL =  hardwareMap.servo.get("Hub1_Servo4");
+
         robotMove robot = new robotMove(hardwareMap);
 
         waitForStart();
 
         robot.forward(0.5, 60);
         robot.stop();
+        
+        outputL.setPosition(1);
+        outputS.setPosition(45);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helperclasses/robotMove.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helperclasses/robotMove.java
@@ -25,16 +25,16 @@ public class robotMove {
         // Change this mapping to however your robot is setup.
 
         // test bot motors
-        motorFL = hardwareMap.dcMotor.get("Hub1_Motor3");
-        motorBL = hardwareMap.dcMotor.get("Hub1_Motor0");
-        motorFR = hardwareMap.dcMotor.get("Hub2_Motor0");
-        motorBR = hardwareMap.dcMotor.get("Hub2_Motor3");
+        // motorFL = hardwareMap.dcMotor.get("Hub1_Motor3");
+        // motorBL = hardwareMap.dcMotor.get("Hub1_Motor0");
+        // motorFR = hardwareMap.dcMotor.get("Hub2_Motor0");
+        // motorBR = hardwareMap.dcMotor.get("Hub2_Motor3");
 
         // comp bot motors
-        //motorFL = hardwareMap.dcMotor.get("H1:M3");
-        //motorBL = hardwareMap.dcMotor.get("H1:M0");
-        //motorFR = hardwareMap.dcMotor.get("H2:M0");
-        //motorBR = hardwareMap.dcMotor.get("H2:M3");
+        motorFL = hardwareMap.dcMotor.get("H1:M3");
+        motorBL = hardwareMap.dcMotor.get("H1:M0");
+        motorFR = hardwareMap.dcMotor.get("H2:M0");
+        motorBR = hardwareMap.dcMotor.get("H2:M3");
 
         // Setup the motors to turn in the correct
         // direction to default to forward motion
