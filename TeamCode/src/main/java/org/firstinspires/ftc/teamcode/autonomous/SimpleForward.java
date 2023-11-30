0240bd3cd3d342cfe40e0bc6b8918895dfc621e0
PowhatanSPEAR13804 <phs-roboticsspear@powhatan.k12.va.us>
2023-11-30 15:15:59 -0500

    Update SimpleForward.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SimpleForward.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SimpleForward.java
@@ -23,7 +23,8 @@ public class SimpleForward extends LinearOpMode{
 
         robot.forward(0.5, 60);
         robot.stop();
-        
+
+        // idk if the arm needs to be extended first or something
         outputL.setPosition(1);
         outputS.setPosition(45);
     }
