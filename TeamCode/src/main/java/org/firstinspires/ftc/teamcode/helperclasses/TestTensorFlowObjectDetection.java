587e9aea631d838e7d7c429e5028b6e7758a9099
PowhatanSPEAR13804 <128644104+PowhatanSPEAR13804@users.noreply.github.com>
2023-12-02 08:34:14 -0500

    Updated Tensorflow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helperclasses/TestTensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helperclasses/TestTensorFlowObjectDetection.java
@@ -56,13 +56,13 @@ public class TestTensorFlowObjectDetection extends LinearOpMode {
 
     // TFOD_MODEL_ASSET points to a model file stored in the project Asset location,
     // this is only used for Android Studio when using models in Assets.
-    private static final String TFOD_MODEL_ASSET = "RedObjectIdentification.tflite";
+    private static final String TFOD_MODEL_ASSET = "model_20231202_083115.tflite";
     // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
     // this is used when uploading models directly to the RC using the model upload interface.
     private static final String TFOD_MODEL_FILE = "/Internal shared storage/Download/RedObjectIdentification.tflite";
     // Define the labels recognized in the model for TFOD (must be in training order!)
     private static final String[] LABELS = {
-       "Red Game Object",
+       "Red Cube","Blue Cube"
     };
 
     /**
