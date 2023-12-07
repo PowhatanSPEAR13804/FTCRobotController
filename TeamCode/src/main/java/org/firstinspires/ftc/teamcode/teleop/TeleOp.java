22c7fcbedc16c77293d951dabea1fa52cd8e139a
PowhatanSPEAR13804 <128644104+PowhatanSPEAR13804@users.noreply.github.com>
2023-12-07 14:40:56 -0500

    Update TeleOp.java
    fixed viper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp.java
@@ -32,9 +32,9 @@ public class TeleOp extends LinearOpMode {
         Servo intakeRoller = hardwareMap.servo.get("Hub2_Servo3");
 
         //Positional servos
-        Servo launch =  hardwareMap.servo.get("Hub1_Servo3");
+        Servo hangingS =  hardwareMap.servo.get("Hub1_Servo3");
         Servo outputS =  hardwareMap.servo.get("Hub1_Servo5");
-        Servo hangingS =  hardwareMap.servo.get("Hub2_Servo1");
+        Servo launch =  hardwareMap.servo.get("Hub2_Servo1");
         Servo hook = hardwareMap.servo.get("Hub2_Servo2");
 
         //linear servo
@@ -103,10 +103,10 @@ public class TeleOp extends LinearOpMode {
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
+            double frontLeftPower = (y + x - rx) / denominator;
+            double backLeftPower = (y - x - rx) / denominator;
+            double frontRightPower = (y - x + rx) / denominator;
+            double backRightPower = (y + x + rx) / denominator;
 
             motorFrontLeft.setPower(frontLeftPower);
             motorBackLeft.setPower(backLeftPower);
@@ -196,7 +196,7 @@ public class TeleOp extends LinearOpMode {
                 on = true;
                 viperPower = Math.max(-1, viperPower - 0.1);
             } else {
-                if(viper.getCurrentPosition() < -1200)
+                if(viper.getCurrentPosition() < -1500)
                     viperPower = 0.5;
                 else
                     viperPower = 0;
@@ -275,7 +275,7 @@ public class TeleOp extends LinearOpMode {
 
                 outputS.setPosition(outputSPosition);
             }
-            hangingS.setPosition((-Math.abs(gamepad2.left_stick_y) + 0.5));
+           // hangingS.setPosition((-Math.abs(gamepad2.left_stick_y) + 0.5));
             if (gamepad1.a || gamepad2.a) {
                 hangingM.setPower(1);
             } else if (gamepad2.x) {
