dfd746319a532636a6c93af1822108c5e8ad7d2c
Samuel Favreau <puttys.favreau@gmail.com>
2023-12-07 16:29:29 -0500

    Formatted ControllerTest, changed drone launch servo in ServoMotorDeclarations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ControllerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ControllerTest.java
@@ -5,10 +5,10 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import java.util.Locale;
 
-//@Disabled
-//safety :)
+//allows us find dysfunctional controllers
 
 @TeleOp(name="ControllerTest", group="Tests")
+
 public class ControllerTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
@@ -16,57 +16,90 @@ public class ControllerTest extends LinearOpMode {
 
         if (isStopRequested()) return;
 
+        //controller section strings
         String sDpad;
         String sLeftStick;
         String sRightStick;
         String sFaceButtons;
-        String sSB;
+        String sStartBack;
         String sBumpers;
         String sTriggers;
 
         while (opModeIsActive()) {
 
             telemetry.addLine("Press mode button to swap dpad and left stick directional inputs, and up on a stick is negative.");
+
+
+            /*
+            since each section is functionally identical, I will only explain one.
+
+            sets the string to display four defined parameters that we then set to the appropriate
+            controller input
+             */
             sDpad = String.format(Locale.US, "dpad: up(%d), down(%d), left(%d), right(%d)",
                     gamepad1.dpad_up ? 1 : 0,
                     gamepad1.dpad_down ? 1 : 0,
                     gamepad1.dpad_left ? 1 : 0,
                     gamepad1.dpad_right ? 1 : 0);
+
+            //add the section line
             telemetry.addLine("\n"+sDpad);
+
+
 
             sLeftStick = String.format(Locale.US, "left stick: button(%d), xAxis(%.1f), yAxis(%.1f)",
                     gamepad1.left_stick_button ? 1 : 0,
                     gamepad1.left_stick_x,
                     gamepad1.left_stick_y);
+
             telemetry.addLine("\n"+sLeftStick);
+
+
 
             sRightStick = String.format(Locale.US, "right stick: button(%d), xAxis(%.1f), yAxis(%.1f)",
                     gamepad1.right_stick_button ? 1 : 0,
                     gamepad1.right_stick_x,
                     gamepad1.right_stick_y);
+
             telemetry.addLine("\n"+sRightStick);
+
+
 
             sFaceButtons = String.format(Locale.US, "face buttons: a(%d), b(%d), x(%d), y(%d)",
                     gamepad1.a ? 1 : 0,
                     gamepad1.b ? 1 : 0,
                     gamepad1.x ? 1 : 0,
                     gamepad1.y ? 1 : 0);
+
             telemetry.addLine("\n"+sFaceButtons);
 
-            sSB = String.format(Locale.US, "start(%d), back(%d)",
+
+
+            sStartBack = String.format(Locale.US, "start(%d), back(%d)",
                     gamepad1.start ? 1 : 0,
                     gamepad1.back ? 1 : 0);
-            telemetry.addLine("\n"+sSB);
+
+            telemetry.addLine("\n"+sStartBack);
+
+
 
             sBumpers = String.format(Locale.US, "bumpers: lb(%d), rb(%d)",
                     gamepad1.left_bumper ? 1 : 0,
                     gamepad1.right_bumper ? 1 : 0);
+
             telemetry.addLine("\n"+sBumpers);
+
+
 
             sTriggers = String.format(Locale.US, "triggers: lt(%.1f), rt(%.1f)",
                     gamepad1.left_trigger,
                     gamepad1.right_trigger);
+
             telemetry.addLine("\n"+sTriggers);
+
+
+
+            //update all our lines on the driver hub screen
             telemetry.update();
         }
     }
