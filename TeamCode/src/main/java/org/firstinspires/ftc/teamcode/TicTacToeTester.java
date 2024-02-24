2c46e315557b54fdc00cdb4f53b85309b63aa0b7
jmonroeharrison <123043892+jmonroeharrison@users.noreply.github.com>
2024-02-23 19:10:03 -0500

    Added the AI
    Added the AI

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TicTacToeTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TicTacToeTester.java
@@ -1,103 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.teamcode.helperclasses.buttonClick;
-
-//@Disabled
-//safety :)
-
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="TicTacToe", group="TeleOp")
-public class TicTacToe extends LinearOpMode {
-
+public class TicTacToeTester {
     static final char player = 'o';
     static final char opponent = 'x';
 
-    @Override
-    public void runOpMode() throws InterruptedException {
 
-        // Declare our motors
-        // Make sure your ID's match your configuration
-        DcMotor xMotor = hardwareMap.dcMotor.get("M0");
-        DcMotor yMotor = hardwareMap.dcMotor.get("M3");
+    public static void main(String[] args) {
+        char board[] = {'x', 'o', 'x' ,
+                        'o', 'o', 'x',
+                        '_', '_', '_'};
 
-        char[] board = new char[]{'_', '_', '_',
-                                  '_', '_', '_',
-                                  '_', '_', '_'};
+        int bestMove = findBestMove(board);
+        int[] movePosition = moveToCoordinates(bestMove);
 
-        //linear servo
-        Servo pickupLinearServo = hardwareMap.servo.get("S4");
-        Servo RotationServo = hardwareMap.servo.get("S5");
-
-        // Reverse the right side motors
-        // Reverse left motors if you are using NeveRests
-
-        xMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        yMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        xMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        yMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        xMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        yMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        RotationServo.setPosition(0);
-
-
-        waitForStart();
-
-        if (isStopRequested()) return;
-
-        while (opModeIsActive()) {
-
-
-            if (gamepad1.dpad_left && yMotor.getCurrentPosition() >= 1000) {
-                yMotor.setPower(-1);
-            } else if (gamepad1.dpad_right && yMotor.getCurrentPosition() <= 20000) {
-                yMotor.setPower(1);
-            } else {
-                yMotor.setPower(0);
-            }
-            if (gamepad1.dpad_up && xMotor.getCurrentPosition() <= 40000) {
-                xMotor.setPower(1);
-            } else if (gamepad1.dpad_down && xMotor.getCurrentPosition() >= 1000) {
-                xMotor.setPower(-1);
-            } else {
-                xMotor.setPower(0);
-            }
-
-            if (gamepad1.x) {
-                pickupLinearServo.setPosition(1);
-            } else if (gamepad1.a) {
-                pickupLinearServo.setPosition(0);
-            }
-
-            if (gamepad1.y) {
-                RotationServo.setPosition((85.0 / 270.0));
-            } else if (gamepad1.b) {
-                RotationServo.setPosition(0);
-            }
-
-
-            telemetry.addLine("xMotor power: " + xMotor.getPower());
-            telemetry.addLine("yMotor power: " + yMotor.getPower());
-            telemetry.addLine("xMotor pos: " + xMotor.getCurrentPosition());
-            telemetry.addLine("yMotor pos: " + yMotor.getCurrentPosition());
-
-            telemetry.addLine("pickupServo: " + pickupLinearServo.getPosition());
-            telemetry.addLine("RotServo: " + RotationServo.getPosition());
-            telemetry.addLine("pickupServo: " + pickupLinearServo.getPosition());
-            telemetry.addLine("RotServo: " + RotationServo.getPosition());
-
-            telemetry.update();
-
-            findBestMove(board);
-
-        }
+        System.out.printf("The Optimal Move is: " + bestMove);
+        System.out.printf("ROW: " + movePosition[0] + "COL: " + movePosition[1]);
     }
 
-    public int[] findBestMove(char[] board) {
+    public static int findBestMove(char[] board) {
         int moveIndex = -1;
         int moveVal = Integer.MIN_VALUE;
         int bestVal = Integer.MIN_VALUE;
@@ -115,19 +35,10 @@ public class TicTacToe extends LinearOpMode {
             }
         }
 
-        int[] movePosition = moveToCoordinates(moveIndex);
-        return movePosition;
+        return moveIndex;
     }
 
-    public static Boolean isMovesLeft(char[] board) {
-        for (int i = 0; i < 9; i++)
-                if (board[i] == '_')
-                    return true;
-        return false;
-    }
-
-
-    public int[] moveToCoordinates(int moveIndex) {
+    public static int[] moveToCoordinates(int moveIndex) {
         int[] movePosition = new int[2];
         switch (moveIndex) {
             //using temporary x and y values.
@@ -154,6 +65,13 @@ public class TicTacToe extends LinearOpMode {
                 movePosition = new int[]{-1, -1};
         }
         return movePosition;
+    }
+
+    public static Boolean isMovesLeft(char[] board) {
+        for (int i = 0; i < 9; i++)
+            if (board[i] == '_')
+                return true;
+        return false;
     }
 
     public static int evaluate(char[] board) {
@@ -227,7 +145,7 @@ public class TicTacToe extends LinearOpMode {
                     best = Math.min(best, minimax(board, depth + 1, !isMax));
                     // undo move
                     board[i] = '_';
-                    }
+                }
             }
             return best;
         }
