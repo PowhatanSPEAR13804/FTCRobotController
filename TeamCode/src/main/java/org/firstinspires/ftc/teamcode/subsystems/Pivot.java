4978f698db1695ea15d991d1a457711ea615dd60
Monroe Harrison <jmonroe08@icloud.com>
2024-09-16 16:33:47 -0400

    Added telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Pivot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Pivot.java
@@ -6,6 +6,8 @@ import com.arcrobotics.ftclib.controller.wpilibcontroller.ArmFeedforward;
 import com.arcrobotics.ftclib.hardware.motors.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class Pivot extends SubsystemBase {
     private static final double TICKS_TO_RADIANS = 0.0;
     private static final double DEGREES_TO_RADIANS = Math.PI/180;
@@ -13,10 +15,13 @@ public class Pivot extends SubsystemBase {
     private final PIDController pid = new PIDController(0.0, 0.0, 0.0);
     private final ArmFeedforward ff = new ArmFeedforward(0.0, 0.0, 0.0);
 
+    private final Telemetry telemetry;
+
     private Double setpoint;
 
-    public Pivot(HardwareMap hardwareMap, String servoID) {
+    public Pivot(HardwareMap hardwareMap, String servoID, Telemetry tl) {
         pivotServo = new CRServo(hardwareMap, servoID);
+        telemetry = tl;
     }
 
     public void rotateWithPID(double position) {
@@ -33,5 +38,6 @@ public class Pivot extends SubsystemBase {
         if(setpoint != null) {
             pivotServo.set(ff.calculate(setpoint, 0.0) + pid.calculate(pivotServo.getCurrentPosition() * TICKS_TO_RADIANS, setpoint));
         }
+        telemetry.addData("Arm angle ", pivotServo.getCurrentPosition());
     }
 }
