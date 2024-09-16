6d1468d777befd65a733d3c764e310df413f20fe
Monroe Harrison <jmonroe08@icloud.com>
2024-09-16 16:30:41 -0400

    fixed error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Archive/TwentyOneTwentyTwo/Teleop_21_22.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Archive/TwentyOneTwentyTwo/Teleop_21_22.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Archive.twentyonetwentytwo;
+package org.firstinspires.ftc.teamcode.Archive.TwentyOneTwentyTwo;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
