c0257d51b78c38cc46787e052713ca09a53089a3
Monroe Harrison <jmonroe08@icloud.com>
2024-09-16 16:30:13 -0400

    Coded pivot subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Pivot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Pivot.java
@@ -1,4 +1,37 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class Pivot {
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.controller.wpilibcontroller.ArmFeedforward;
+import com.arcrobotics.ftclib.hardware.motors.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Pivot extends SubsystemBase {
+    private static final double TICKS_TO_RADIANS = 0.0;
+    private static final double DEGREES_TO_RADIANS = Math.PI/180;
+    private final CRServo pivotServo;
+    private final PIDController pid = new PIDController(0.0, 0.0, 0.0);
+    private final ArmFeedforward ff = new ArmFeedforward(0.0, 0.0, 0.0);
+
+    private Double setpoint;
+
+    public Pivot(HardwareMap hardwareMap, String servoID) {
+        pivotServo = new CRServo(hardwareMap, servoID);
+    }
+
+    public void rotateWithPID(double position) {
+        setpoint = position * DEGREES_TO_RADIANS;
+    }
+
+    public void rotate(double speed) {
+        setpoint = null;
+        pivotServo.set(speed);
+    }
+
+    @Override
+    public void periodic() {
+        if(setpoint != null) {
+            pivotServo.set(ff.calculate(setpoint, 0.0) + pid.calculate(pivotServo.getCurrentPosition() * TICKS_TO_RADIANS, setpoint));
+        }
+    }
 }
