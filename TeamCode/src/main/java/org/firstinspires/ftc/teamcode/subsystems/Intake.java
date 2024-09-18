47ab300f7f1ce03bbb380a613b50fd48855b6026
Morgan Hand <night.dragonjr@outlook.com>
2024-09-18 15:22:03 -0400

    Added the guts to Intake.java and removed redundant file.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,4 +1,22 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class Intake {
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.motors.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake extends SubsystemBase {
+    private final CRServo intakeServo;
+
+    public Intake(HardwareMap hardwareMap, String servoID) {
+        intakeServo = new CRServo(hardwareMap, servoID);
+        intakeServo.setInverted(false);
+    }
+
+    public void rotateIn() {
+        intakeServo.set(1.0);
+    }
+    public void rotateOut() {
+        intakeServo.set(-1.0);
+    }
 }
